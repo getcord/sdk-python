39ab75f6ac54dfbc31ff07db53a24d0e8d12bfed
nickfil22 <nickfil@cord.com>
2023-09-19 10:51:05 +0100

    Cleaning up

--- server/server.py
+++ server/server.py
@@ -1,29 +1,47 @@
 import datetime
-from enum import Enum
 import json
 import jwt
 import requests
+
+CORD_ENDPOINT = "https://api.cord.com/v1/"
+
+from enum import Enum
 
 class status(Enum):
     ACTIVE = "active"
     DELETED = "deleted"
 
 class platformUserVariables:
-    def __init__(self, email: str, name: str = None, status: status = None, profile_picture_url: str = None, first_name: str = None, last_name: str = None, id: str = None):
+    def __init__(self, email: str, name: str = None, status: status = None, profile_picture_url: str = None, first_name: str = None, last_name: str = None):
         self.email = email
         self.name = name
         self.status = status
         self.profile_picture_url = profile_picture_url
         self.first_name = first_name
         self.last_name = last_name
-        self.id = id
 
 class platformOrganizationVariables:
-    def __init__(self, name: str, status: status = None, members: list[str] = None, id: str = None):
+    def __init__(self, name: str, status: status = None, members: list[str] = None):
         self.name = name
         self.status = status
         self.members = members
+
+class clientPlatformUserVariables:
+    def __init__(self, id: str, email: str, name: str = None, status: status = None, profile_picture_url: str = None, first_name: str = None, last_name: str = None):
         self.id = id
+        self.email = email
+        self.name = name
+        self.status = status
+        self.profile_picture_url = profile_picture_url
+        self.first_name = first_name
+        self.last_name = last_name
+
+class clientPlatformOrganizationVariables:
+    def __init__(self, id: str, name: str, status: status = None, members: list[str] = None):
+        self.id = id
+        self.name = name
+        self.status = status
+        self.members = members
 
 def toJson(obj):
     return json.dumps(obj, default=lambda o: dict((key, value) for key, value in o.__dict__.items() if value),
@@ -53,41 +71,46 @@ def get_server_auth_token(app_id: str, secret: str):
         algorithm='HS512'
     )
 
-def sync_cord_user(app_id: str, secret: str, user_id: str, user: platformUserVariables):
-    auth_token: str = get_server_auth_token(app_id, secret)
-
-    headers = {
-        "Authorization": "Bearer {}".format(auth_token),
-        "Content-Type": "application/json"
-    }
-    response = requests.put("https://api.cord.com/v1/users/" + user_id, headers=headers, data=toJson(user))
-
-    return response.json()
-
-def sync_cord_organization(app_id: str, secret: str, org_id: str, organization: platformOrganizationVariables):
-    auth_token: str = get_server_auth_token(app_id, secret)
-
-    headers = {
-        "Authorization": "Bearer {}".format(auth_token),
-        "Content-Type": "application/json"
-    }
-    response = requests.put("https://api.cord.com/v1/organizations/" + org_id, headers=headers, data=toJson(organization))
-
-    return response.json()
-
-def batch_sync_cord_users_and_organizations(app_id: str, secret: str, users: list[platformUserVariables], organizations: list[platformOrganizationVariables]):
-    auth_token: str = get_server_auth_token(app_id, secret)
-
-    headers = {
-        "Authorization": "Bearer {}".format(auth_token),
-        "Content-Type": "application/json"
-    }
-    request_body = "{{ \"organizations\": {}, \"users\": {} }}".format(toJson(organizations), toJson(users))
-    response = requests.post("https://api.cord.com/v1/batch", headers=headers, data=request_body)
-    
-    return response.json()
+class CordClient:
+    def __init__(self, app_id, secret):
+        self.app_id = app_id
+        self.secret = secret
 
 
-print(sync_cord_user("1ac6151f-622e-4b71-9f4d-fbfd3339ba39", "730f61aa965fb520a59a5fd63aa2b092", "1", platformUserVariables("nick@cord.com")))
-print(sync_cord_organization("1ac6151f-622e-4b71-9f4d-fbfd3339ba39", "730f61aa965fb520a59a5fd63aa2b092", "1", platformOrganizationVariables("org1")))
-print(batch_sync_cord_users_and_organizations("1ac6151f-622e-4b71-9f4d-fbfd3339ba39", "730f61aa965fb520a59a5fd63aa2b092", [ platformUserVariables("nick@cord.com", None, None, None, None, None, "1") ], [ platformOrganizationVariables("org1", None, None, "1")]))
+    def sync_cord_user(self, user: clientPlatformUserVariables):
+        auth_token: str = get_server_auth_token(self.app_id, self.secret)
+        userId = user.id
+        delattr(user, "id")
+
+        headers = {
+            "Authorization": "Bearer {}".format(auth_token),
+            "Content-Type": "application/json"
+        }
+        response = requests.put(CORD_ENDPOINT + "users/" + userId, headers=headers, data=toJson(user))
+
+        return response.json()
+
+    def sync_cord_organization(self, organization: clientPlatformOrganizationVariables):
+        auth_token: str = get_server_auth_token(self.app_id, self.secret)
+        orgId = organization.id
+        delattr(organization, "id")
+
+        headers = {
+            "Authorization": "Bearer {}".format(auth_token),
+            "Content-Type": "application/json"
+        }
+        response = requests.put(CORD_ENDPOINT + "organizations/" + orgId, headers=headers, data=toJson(organization))
+
+        return response.json()
+
+    def batch_sync_cord_users_and_organizations(self, users: list[clientPlatformUserVariables], organizations: list[platformOrganizationVariables]):
+        auth_token: str = get_server_auth_token(self.app_id, self.secret)
+
+        headers = {
+            "Authorization": "Bearer {}".format(auth_token),
+            "Content-Type": "application/json"
+        }
+        request_body = "{{ \"organizations\": {}, \"users\": {} }}".format(toJson(organizations), toJson(users))
+        response = requests.post(CORD_ENDPOINT + "batch", headers=headers, data=request_body)
+        
+        return response.json()
